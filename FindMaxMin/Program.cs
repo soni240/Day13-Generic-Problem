d7c2212a2fbe5318a93d21a54ffa45c967fd123b
soni240 <s.fegade21@gmail.com>
2022-06-28 19:02:57 +0530

    [Gunavanti]Add:finding maximum number

---
+++ FindMaxMin/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace FindMaxMin
+{
+
+
+
+    public class Program
+    {
+        public int FindMaximum(int first, int second, int third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
+            {
+                return third;
+            }
+            else
+            {
+                Console.WriteLine("All three having same number");
+                return -1;
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            int maxValue = obj.FindMaximum(70, 35, 56);
+            Console.WriteLine(maxValue);
+            Console.ReadLine();
+        }
+
+
+    }
+
+}
+
