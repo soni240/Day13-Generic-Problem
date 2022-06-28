ae13bfd244fc1996fc4765da5778eb2bb613c383
soni240 <s.fegade21@gmail.com>
2022-06-28 19:43:52 +0530

    [Gunavanti]Add:using maximum method

--- FindMaxMin/Program.cs
+++ FindMaxMin/Program.cs
@@ -1,20 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System;
-
-namespace FindMaxValue
-{
-
-
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Generics and Unit testing Problem!!!");
-
-
-
-
-        }
-    }
-}
+Console.WriteLine("Hello, World!");
