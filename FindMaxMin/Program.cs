a2a36998525b8cb055ce6d07d41f1a1b2a3826ab
soni240 <s.fegade21@gmail.com>
2022-06-28 19:38:06 +0530

    [Gunavanti]Add:using sorting more than 3 parameters

--- FindMaxMin/Program.cs
+++ FindMaxMin/Program.cs
@@ -6,48 +6,15 @@ namespace FindMaxValue
 
 
 
-    public class Program
+    class Program
     {
-        // finding maximum string
-
-        public string FindMaximumString(string first, string second, string third)
-        {
-            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
-            {
-                return first;
-            }
-            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
-            {
-                return second;
-            }
-            else if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
-            {
-                return third;
-            }
-            else
-            {
-                Console.WriteLine("All three having same number");
-                return default;
-            }
-        }
-
-
-
         static void Main(string[] args)
         {
-            Program obj = new Program();
-            Console.WriteLine("Maximum value is : " + obj.FindMaximumString("Apple", "Banana", "Peach"));
+            Console.WriteLine("Welcome to Generics and Unit testing Problem!!!");
 
-            Console.ReadLine();
+
+
+
         }
-
-
     }
-
-
-
-
-
-
 }
-
---
+++ FindMaxMin/SortingClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxMin
+{
+    public class SortingClass<T> where T : IComparable
+    {
+        public T[] arrayList;
+        private string[] arrString;
+        private float[] arrFloat;
+
+        public SortingClass(T[] arrayList)
+        {
+            this.arrayList = arrayList;
+        }
+
+        public SortingClass(string[] arrString)
+        {
+            this.arrString = arrString;
+        }
+
+        public SortingClass(float[] arrFloat)
+        {
+            this.arrFloat = arrFloat;
+        }
+
+        public T[] Sort(T[] arrayList)
+        {
+            Array.Sort(arrayList);
+            return arrayList;
+        }
+        public T MaximumValueFunction(T[] arrayList)
+        {
+            var sortarrayval = Sort(arrayList);
+            return sortarrayval[arrayList.Length - 1];
+        }
+        public T MaxMethod()
+        {
+            T maxValue = this.MaximumValueFunction(arrayList);
+            return maxValue;
+        }
+    }
+}
+
+
+    
+
+  
